80df47c6037125ccb4b7fc725853139faf671b2f
Alexander Smarus <bender@readdle.com>
2023-09-23 01:17:10 +0300

    Create Dummy.swift

---
+++ Dummy.swift
@@ -0,0 +1,22 @@
+@objc
+protocol DummyProtocol {
+}
+
+class DummyObjectCompat1: NSObjectCompat {
+}
+
+class DummyObjectCompat2: NSObjectCompat {
+}
+
+class DummyObject1: NSObject {
+}
+
+class DummyObject2: NSObject {
+}
+
+class DummyObject3: NSObject {
+}
+
+@objcMembers
+class DummyObject4: NSObject {
+}
