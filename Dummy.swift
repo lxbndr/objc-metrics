0cf07b586695c76c7260e5422e77e6df523dffd3
Alexander Smarus <bender@readdle.com>
2023-09-24 02:05:47 +0300

    Update Dummy.swift

--- Dummy.swift
+++ Dummy.swift
@@ -8,12 +8,10 @@ class DummyObjectCompat1: NSObjectCompat {
 class DummyObjectCompat2: NSObjectCompat {
 }
 
-class DummyObject1: NSObject {
-}
-
 class DummyObject2: NSObject {
 }
 
+@objc
 class DummyObject3: NSObject {
 }
 
